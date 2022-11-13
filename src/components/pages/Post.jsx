ded9eab848e02d23252502eaf7414a69d41877a5
JACEEO <juneeye92@gmail.com>
2022-11-13 15:45:54 +0900

    리액트 배포

---
+++ src/components/pages/Post.jsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { getPost } from "../../api";
+
+function Post() {
+  useEffect(() => {
+    getPost(1).then((res) => console.log(res));
+  }, []);
+
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default Post;
